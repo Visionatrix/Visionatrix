0ac653c14e1929808296742fc6275651cd23dce3
Andrey Borysenko <andrey18106x@gmail.com>
2024-03-30 20:35:45 +0200

    ui updates and fixes, build-client
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -15,19 +15,14 @@ const flowsStore = useFlowsStore()
 <template>
   <AppContainer class="lg:h-dvh">
     <UProgress v-if="flowsStore.$state.loading.flows_available || flowsStore.loading.flows_installed || flowsStore.$state.loading.tasks_history" />
-    <div v-else-if="flowsStore.flows.length > 0"
-      class="h-full flex flex-col items-between justify-between">
-      <div class="flex flex-wrap justify-center items-center">
-        <WorkflowListItem v-for="flow in flowsStore.paginatedFlows" :key="flow.name" :flow="flow" />
-      </div>
-      <div v-if="flowsStore.flows.length > flowsStore.$state.pageSize" class="w-full flex justify-center my-5">
+    <template v-else-if="flowsStore.flows.length > 0">
+      <div v-if="flowsStore.flows.length > flowsStore.$state.pageSize" class="w-full sticky top-1 flex justify-center my-1">
         <UPagination v-model="flowsStore.$state.page" :page-count="flowsStore.$state.pageSize" :total="flowsStore.flows.length" />
       </div>
-    </div>
-    <div v-else>
-      <p class="text-center text-slate-500">No flows available</p>
-    </div>
+      <div class="flex flex-wrap justify-center items-center mb-10">
+        <WorkflowListItem v-for="flow in flowsStore.paginatedFlows" :key="flow.name" :flow="flow" />
+      </div>
+    </template>
+    <p v-else class="text-center text-slate-500">No flows available</p>
   </AppContainer>
 </template>
-
-<style scoped></style>
