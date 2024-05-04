03cb9306c7f3984a71ccdcfd840e58106ad1a6c3
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-05-04 16:24:18 +0300

    refactoring: added `AIResourceModel` pydantic model instead of regular `dict`
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- visionatrix/install_update/update.py
+++ visionatrix/install_update/update.py
@@ -18,14 +18,15 @@ def __progress_callback(name: str, progress: float, error: str) -> None:
 
 def update() -> None:
     logging.info("Updating flows..")
-    avail_flows, avail_flows_comfy = get_available_flows()
-    avail_flows_names = [v["name"] for v in avail_flows]
+    avail_flows_comfy = []
+    avail_flows = get_available_flows(avail_flows_comfy)
+    avail_flows_names = [v.name for v in avail_flows]
     for i in get_installed_flows():
-        if i["name"] in avail_flows_names:
-            v = avail_flows_names.index(i["name"])
+        if i.name in avail_flows_names:
+            v = avail_flows_names.index(i.name)
             install_custom_flow(avail_flows[v], avail_flows_comfy[v], __progress_callback)
         else:
-            logging.warning("`%s` flow not found in repository, skipping update of it.", i["name"])
+            logging.warning("`%s` flow not found in repository, skipping update of it.", i.name)
     if options.BACKEND_DIR:
         logging.info("Updating backend(ComfyUI)..")
         run("git pull".split(), check=True, cwd=options.BACKEND_DIR)
