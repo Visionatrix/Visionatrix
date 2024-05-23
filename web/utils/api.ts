22c917ac552918c4e0daaaa492f4018124ef765b
Andrey Borysenko <andrey18106x@gmail.com>
2024-05-23 14:03:54 +0300

    Add nextcloud support (#73)
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- web/utils/api.ts
+++ web/utils/api.ts
@@ -1,10 +1,14 @@
 export function buildBackendApiUrl() {
 	const config = useRuntimeConfig()
+	let prefix = null
+	if (config.app.baseURL !== '/') {
+		prefix = config.app.baseURL
+	}
 	return config.app.backendApiUrl !== ''
 		? config.app.backendApiUrl
 		: location.port 
 			? `${location.protocol}//${location.hostname}:${location.port}`
-			: `${location.protocol}//${location.hostname}`
+			: `${location.protocol}//${location.hostname}` + (prefix ? prefix : '')
 }
 
 export function buildAuthorization() {
