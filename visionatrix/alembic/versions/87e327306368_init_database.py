7e5ab8ce4984c2edb4caafb1e43baf00f1cba766
Alexander Piskun <bigcat88@icloud.com>
2024-06-12 20:46:57 +0300

    corrected template to make `ruff` happy
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- visionatrix/alembic/versions/87e327306368_init_database.py
+++ visionatrix/alembic/versions/87e327306368_init_database.py
@@ -6,16 +6,16 @@ Create Date: 2024-06-12 20:20:51.613173
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "87e327306368"
-down_revision: Union[str, None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
