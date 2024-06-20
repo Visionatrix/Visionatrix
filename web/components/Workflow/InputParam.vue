5191060bafcfa30edcf49c4706c9ad650f672a90
Andrey Borysenko <andrey18106x@gmail.com>
2024-06-20 16:17:34 +0300

    add edit attention like in ComfyUI, build-client
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- web/components/Workflow/InputParam.vue
+++ web/components/Workflow/InputParam.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { editAttention } from '~/utils/editAttention'
+
 const props = defineProps({
 	inputParam: {
 		type: Object,
@@ -93,6 +95,24 @@ const formGroupHelp = computed(() => {
 onBeforeUnmount(() => {
 	URL.revokeObjectURL(imagePreviewUrl.value)
 })
+
+const textareaInput = ref(null)
+
+// add event listener for textarea keydown for editAttention feature
+onMounted(() => {
+	if (props.inputParam.type === 'text' && textareaInput.value) {
+		// @ts-ignore
+		textareaInput.value.$refs.textarea.addEventListener('keydown', editAttention)
+	}
+})
+
+onBeforeUnmount(() => {
+	if (props.inputParam.type === 'text' && textareaInput.value) {
+		// @ts-ignore
+		textareaInput.value.$refs.textarea.removeEventListener('keydown', editAttention)
+	}
+})
+
 </script>
 
 <template>
@@ -104,6 +124,7 @@ onBeforeUnmount(() => {
 		</template>
 		<template #default>
 			<UTextarea v-if="inputParam.type === 'text'"
+				ref="textareaInput"
 				size="md"
 				:placeholder="inputParam.display_name"
 				:required="!inputParam.optional"
