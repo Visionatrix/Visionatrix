902c1aa3dbb07ea5ec7183889eb7af7c154d9a7c
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-09-10 19:42:16 +0300

    fix: windows: install nvidia torch by default
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

--- scripts/easy_install.py
+++ scripts/easy_install.py
@@ -257,6 +257,10 @@ def install_graphics_card_packages():
             venv_run(pip_install + "torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1")
     elif c == "nvidia":
         print("Installing packages for NVIDIA graphics card...")
+        if sys.platform.lower() == "win32":
+            venv_run(pip_install + "torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu121")  # noqa
+        else:
+            venv_run(pip_install + "torch torchvision torchaudio")
         venv_run(pip_install + "torch torchvision torchaudio")
     else:
         print("Skipping graphics card package installation.")
