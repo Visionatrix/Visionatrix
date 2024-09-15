0ffd68a60ba648fe0062ab530af0ab077a57dbe0
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-09-16 01:21:57 +0300

    restore LogLvl after ComfyUI import (#164)
    discovered by accident after I realized that in my local devsetup I
    always have the Warning level, but I still see the "info" level logs
    
    Signed-off-by: Alexander Piskun <bigcat88@icloud.com>

---
+++ visionatrix/etc.py
@@ -0,0 +1,24 @@
+import logging
+
+
+def get_log_level(log_level_str):
+    """Convert log level string to logging module log level."""
+    log_levels = {
+        "DEBUG": logging.DEBUG,
+        "INFO": logging.INFO,
+        "WARNING": logging.WARNING,
+        "ERROR": logging.ERROR,
+        "CRITICAL": logging.CRITICAL,
+    }
+    return log_levels.get(log_level_str.upper(), logging.INFO)
+
+
+def get_higher_log_level(current_level):
+    level_mapping = {
+        logging.DEBUG: logging.INFO,
+        logging.INFO: logging.WARNING,
+        logging.WARNING: logging.ERROR,
+        logging.ERROR: logging.ERROR,
+        logging.CRITICAL: logging.CRITICAL,
+    }
+    return level_mapping.get(current_level, logging.WARNING)
