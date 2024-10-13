e06608371a9934941c66883dda4ca62fea89eb3b
Andrey Borysenko <andrey18106x@gmail.com>
2024-10-13 03:21:00 +0300

    fix: notifications component
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- web/app.vue
+++ web/app.vue
@@ -9,5 +9,10 @@ useWorkersStore().loadWorkers()
 		<Navbar />
 		<NuxtPage class="w-full min-h-vh py-1.5 z-10" />
 	</div>
-	<UNotifications class="z-5" />
+	<UNotifications :ui="{
+		wrapper: 'fixed flex flex-col justify-end z-[55] pointer-events-none',
+		position: 'bottom-0 end-0',
+		width: 'w-full sm:w-96',
+		container: 'px-4 sm:px-6 py-6 space-y-3 overflow-y-auto',
+	}" />
 </template>
