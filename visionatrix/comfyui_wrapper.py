620a1b9f1dd6a01577f5b2138c22253a5f1f066f
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-12-03 13:33:05 +0200

    removed "cleanup_models" function
    Signed-off-by: bigcat88 <bigcat88@icloud.com>

--- visionatrix/comfyui_wrapper.py
+++ visionatrix/comfyui_wrapper.py
@@ -262,12 +262,6 @@ def interrupt_processing() -> None:
     nodes.interrupt_processing()
 
 
-def cleanup_models(keep_clone_weights_loaded=False) -> None:
-    import comfy  # noqa
-
-    comfy.model_management.cleanup_models(keep_clone_weights_loaded)
-
-
 def unload_all_models() -> None:
     import comfy  # noqa
 
