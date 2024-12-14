70471d2e1653ccc6f3a1844e446c2f2401db95b7
Andrey Borysenko <andrey18106x@gmail.com>
2024-12-14 03:53:18 +0200

    chore(ui): remove not needed anymore notifications ui workaround
    fixed in nuxt/ui update (https://github.com/nuxt/ui/issues/2113)
    
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- web/app.vue
+++ web/app.vue
@@ -9,11 +9,5 @@ useWorkersStore().loadWorkers()
 		<Navbar />
 		<NuxtPage class="w-full min-h-vh py-1.5 z-10" />
 	</div>
-	<UNotifications
-		:ui="{
-			wrapper: 'fixed flex flex-col justify-end z-[55] pointer-events-none',
-			position: 'bottom-0 end-0',
-			width: 'w-full sm:w-96',
-			container: 'px-4 sm:px-6 py-6 space-y-3 overflow-y-auto',
-		}" />
+	<UNotifications />
 </template>
