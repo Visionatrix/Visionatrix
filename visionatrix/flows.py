47ed55be596c1f743d1b04bb02c83d9b1f25f005
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2024-12-23 16:15:31 +0300

    video flows support: added SaveAnimatedWEBP to supported video outputs (#276)
    added the new `image-animated` type of output for 'SaveAnimatedWEBP' node.
    
    ---------
    
    Signed-off-by: bigcat88 <bigcat88@icloud.com>
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>
    Co-authored-by: Andrey Borysenko <andrey18106x@gmail.com>

--- visionatrix/flows.py
+++ visionatrix/flows.py
@@ -43,6 +43,7 @@ CACHE_INSTALLED_FLOWS_EVENT = threading.Condition(CACHE_INSTALLED_FLOWS_LOCK)
 
 SUPPORTED_OUTPUTS = {
     "SaveImage": "image",
+    "SaveAnimatedWEBP": "image-animated",
     "VHS_VideoCombine": "video",
 }
 
