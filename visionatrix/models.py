16faf93032fa97726efeef75346e2a97a3dc404b
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2025-01-16 19:07:09 +0200

    correctly mark models in "zip" archives as installed
    Signed-off-by: bigcat88 <bigcat88@icloud.com>

--- visionatrix/models.py
+++ visionatrix/models.py
@@ -362,6 +362,7 @@ def does_model_exist_in_fs(
                 check_result = False
                 break
             if check_result:
+                db_queries.complete_model_progress_install(model.name, flow_name)
                 return True
 
     return lookup_for_model_file(model, model_possible_directories, flow_name, model_progress_install)
