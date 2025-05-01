bff6527dad1f8dafbf54a09f80c665391ce51382
Alexander Piskun <13381981+bigcat88@users.noreply.github.com>
2025-05-01 08:24:02 +0300

    feat(integrations): allow to exclude nodes during install (#393)
    Signed-off-by: bigcat88 <bigcat88@icloud.com>

--- visionatrix/install_update/custom_nodes.py
+++ visionatrix/install_update/custom_nodes.py
@@ -36,7 +36,7 @@ def install_base_custom_nodes() -> None:
             "install",
             f"--user-directory={options.USER_DIR}",
             "--mode=cache",
-            *list(BASIC_NODE_LIST.keys()),
+            *get_basic_nodes_to_install(),
         ],
         env=clone_env,
         check=True,
@@ -50,7 +50,7 @@ def update_base_custom_nodes() -> None:
         if i.startswith("https://"):
             basic_nodes_list.append(i.rstrip("/").split("/")[-1])
         else:
-            basic_nodes_list.append(i.split("@", maxsplit=1)[0])
+            basic_nodes_list.append(i)
     clone_env = os.environ.copy()
     clone_env["COMFYUI_PATH"] = str(Path(options.COMFYUI_DIR).resolve())
     clone_env["COMFYUI_MODEL_PATH"] = str(Path(options.MODELS_DIR).resolve())
@@ -66,7 +66,7 @@ def update_base_custom_nodes() -> None:
             "install",
             f"--user-directory={options.USER_DIR}",
             "--mode=cache",
-            *list(BASIC_NODE_LIST.keys()),
+            *get_basic_nodes_to_install(),
         ],
         env=clone_env,
         check=True,
@@ -88,3 +88,34 @@ def install_wheel(url: str, file_name: str):
         run([sys.executable, "-m", "pip", "install", temp_file_path], check=True)
     finally:
         os.remove(temp_file_path)
+
+
+def get_basic_nodes_to_install() -> list[str]:
+    """Generates the list of node identifiers (name@version or URL) for ComfyUI-Manager install command."""
+
+    install_args = []
+    excluded_nodes = options.INSTALL_EXCLUDE_NODES_SET
+    if excluded_nodes:
+        LOGGER.info("Nodes excluded from installation via environment variable: %s", excluded_nodes)
+
+    LOGGER.debug("Processing BASIC_NODE_LIST for installation...")
+    for key, value_dict in BASIC_NODE_LIST.items():
+        if key in excluded_nodes:
+            LOGGER.debug("  - Skipping excluded node: %s", key)
+            continue
+
+        if key.startswith("https://"):
+            install_args.append(key)
+            LOGGER.debug("  - Adding URL: %s", key)
+        else:
+            version = value_dict.get("version")
+            if version:
+                arg = f"{key}@{version}"
+                install_args.append(arg)
+                LOGGER.debug("  - Adding versioned package: %s", arg)
+            else:
+                install_args.append(key)
+                LOGGER.debug("  - Adding package (no version specified): %s", key)
+
+    LOGGER.info("Final list of nodes prepared for ComfyUI-Manager install: %s", install_args)
+    return install_args
